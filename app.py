414ef972d8e52f0b8a09275af64e4a0e7f99aa09
Lukasz Kuczynski <lukasz.kuczynski@bp.com>
2025-12-15 12:41:47 +0100

    request astext

--- app.py
+++ app.py
@@ -250,10 +250,9 @@ def get_items():
 @cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:3000"])
 @requires_auth
 def add_items():
-    data = json.loads(request.get_json())
+    data = json.loads(request.get_data(as_text=True))
     print(request.headers.get("Content-Type"))
     print(data)
-    print(type(data))
     if data and "id" in data:
         new_id = data["id"]
     else:
